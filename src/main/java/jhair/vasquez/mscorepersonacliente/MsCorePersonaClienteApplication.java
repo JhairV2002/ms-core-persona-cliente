bbcedd2609948a6529678f71d85c61380cb308f3
JhairV2002 <jhairvasquez2002@gmail.com>
2025-02-23 18:51:59 -0500

    first commit

---
+++ src/main/java/jhair/vasquez/mscorepersonacliente/MsCorePersonaClienteApplication.java
@@ -0,0 +1,13 @@
+package jhair.vasquez.mscorepersonacliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCorePersonaClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCorePersonaClienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jhair/vasquez/mscorepersonacliente/MsCorePersonaClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package jhair.vasquez.mscorepersonacliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCorePersonaClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
